35ca5e890c79fcd887c619c2084541b84406dee4
王迎震(e-Yingzhen.Wang) <e-Yingzhen.Wang@caocaoglobal.com>
2023-10-12 15:54:47 +0800

    内容

--- lead-content/src/containers/list/index.js
+++ lead-content/src/containers/list/index.js
@@ -1,54 +1,54 @@
 import { Button, Form, Input, message } from "antd";
 import React from "react";
 
-class List extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            data:"134",
-            aa:undefined,
-            number:[],
-        }
+class List extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: "134",
+      aa: undefined,
+      number: [],
     }
-    // queryData=()=>{
-    //     // const {} = this.props.form
-    //     const [form] = Form.useForm();
-    //     // if(getFieldValue("username")|| getFieldValue("number")){
-    //         const data =form.getFieldValue("username")
-    //         const number =form.getFieldValue("number")
-    //         console.log(data)
-    //         console.log(number)
-    //     // }else{
-    //         // message.error("请填写")
-    //     // }
+  }
+  // queryData=()=>{
+  //     // const {} = this.props.form
+  //     const [form] = Form.useForm();
+  //     // if(getFieldValue("username")|| getFieldValue("number")){
+  //         const data =form.getFieldValue("username")
+  //         const number =form.getFieldValue("number")
+  //         console.log(data)
+  //         console.log(number)
+  //     // }else{
+  //         // message.error("请填写")
+  //     // }
 
-    // }
-     onFinish = (values) => {
-        console.log('Received values from form:', values);
-        if(values.data && values.number){
-            const data = values.data
-            const array = []
-            const number = values.number
-            const aa = data/number
-            this.setState({
-                aa,
-                number:array.length=number
-            })
-        }else{
-            message.error("请填写内容")
-        }
+  // }
+  onFinish = (values) => {
+    console.log('Received values from form:', values);
+    if (values.data && values.number) {
+      const data = values.data
+      const array = []
+      const number = values.number
+      const aa = data / number
+      this.setState({
+        aa,
+        number: array.length = number
+      })
+    } else {
+      message.error("请填写内容")
+    }
 
-      };
-    
-      
-    render(){
-        const {aa ,number} =this.state
-        console.log(aa)
-        console.log(number)
-        // const { getFieldDecorator } = this.props.form;
-        return(
-            <div>
-                {/* <Form>
+  };
+
+
+  render() {
+    const { aa, number } = this.state
+    console.log(aa)
+    console.log(number)
+    // const { getFieldDecorator } = this.props.form;
+    return (
+      <div>
+        {/* <Form>
                     <Form.Item label="内容">
                     {getFieldDecorator('data', {
               rules: [{
@@ -82,23 +82,23 @@ class List extends React.Component{
                     <Button onClick={()=>{this.queryData()}}>查看</Button>
                 </Form> */}
 
-<Form onFinish={this.onFinish}>
-            <Form.Item label="Name" name="data">
-              <Input />
-            </Form.Item>
-      
-            <Form.Item label="Email" name="number">
-              <Input />
-            </Form.Item>
-      
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-            </div>
-        )
-    }
+        <Form onFinish={this.onFinish}>
+          <Form.Item label="Name" name="data">
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Email" name="number">
+            <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    )
+  }
 }
 export default List;
